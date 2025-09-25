ca74eb9705191316a025af9b592b8aa07d573f8d
Kneerav <Nirav.maniar@acu.edu.au>
2025-09-25 18:10:06 +1000

    Added data from Verheul 2024

---
+++ data/data_verheul_2024.R
@@ -0,0 +1,21 @@
+# In R/data_verheul_2024.R
+
+#' Example Dataset: data_verheul_2024
+#'
+#' An example dataset from Verheul et al., 2024
+#'
+#' @format A data frame with 422685 rows and 7 variables:
+#' \describe{
+#'   \item{value}{Numeric value}
+#'   \item{variable}{Character variable}
+#'   \item{trial}{Character trial}
+#'   \item{participant}{Character participant}
+#'   \item{data_type}{Character data_type}
+#'   \item{leg}{Character leg}
+#'   \item{stance}{Numeric variable}
+#' }
+#' @source Verheul et al., 2024
+#' @references
+#' Verheul J, Harper D, Robinson MA. Forces experienced at different levels of the musculoskeletal system during horizontal decelerations. J Sports Sci. 2024 Dec;42(23):2242-2253. doi: 10.1080/02640414.2024.2428086.
+
+"data_verheul_2024"
